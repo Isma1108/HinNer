bdd0297a9fcfa978cfeb5ba16c1cb3b3775c8f34
Isma1108 <ismaelelbasli@gmail.com>
2024-05-14 00:12:17 +0200

    Fetes tasques 1 i 2

--- hm.g4
+++ hm.g4
@@ -2,12 +2,43 @@
 
 grammar hm;
 
+///////////////////
+/// PARSER RULES///
+///////////////////
+
 root : expr             
      ;
 
-expr : NUM
+expr : aplicacio 	
+     | abstraccio 
+     | variable     
+     | natural      
      ;
 
-NUM : [0-9]+ ;
+
+aplicacio : '(' abstraccio ')' expr # aplAbs
+          | aplicacio expr          # aplRec
+          | OP expr 		    # aplOp		
+          ;
+
+abstraccio: '\\' variable '->' expr;
+
+variable : VAR;
+
+natural  : NAT;
+
+
+//////////////////
+/// LEXER RULES///
+//////////////////
+
+VAR : LLETRA+;
+NAT : DIGIT+;
+OP  : '(' ('+' | '-' | '*' | '/') ')';
+
+
+// Fragments
+fragment LLETRA : 'a'..'z'|'A'..'Z';
+fragment DIGIT  : '0'..'9';
 
 WS  : [ \t\n\r]+ -> skip ;
--- hm.py
+++ hm.py
@@ -1,46 +1,96 @@
-import streamlit as st
-import pandas as pd
+from __future__ import annotations
 from antlr4 import *
 from hmLexer import hmLexer
 from hmParser import hmParser
-
+from hmVisitor import hmVisitor
 from graphviz import Graph
+from dataclasses import dataclass
+import streamlit as st
 
-#input_stream = InputStream(input('? ')) #-> una linia
-#input_stream = StdinStream()
-#lexer = hmLexer(input_stream)
-#token_stream = CommonTokenStream(lexer)
-#parser = hmParser(token_stream)
-#tree = parser.root()
 
-#print(parser.getNumberOfSyntaxErrors(), 'errors de sintaxi.')
-#print(tree.toStringTree(recog=parser))
+class Empty:
+    pass
 
-def generate_dot_tree():
+@dataclass
+class Node:
+    val: str
+    left: Tree
+    right: Tree
+
+Tree = Node | Empty
+
+
+class SemanticTreeBuilder(hmVisitor):
+
+    def visitAplRec(self, ctx:hmParser.AplRecContext):
+        nodeApl = self.visit(ctx.aplicacio())
+        nodeExpr = self.visit(ctx.expr())
+        return Node('@', nodeApl, nodeExpr)
+
+
+    def visitAplOp(self, ctx:hmParser.AplOpContext):
+        nodeOp = Node(ctx.OP().getText(), Empty(), Empty())
+        nodeExpr = self.visit(ctx.expr())
+        return Node('@', nodeOp, nodeExpr)
+
+
+    def visitAplAbs(self, ctx:hmParser.AplAbsContext):
+        nodeAbs = self.visit(ctx.abstraccio())
+        nodeExpr = self.visit(ctx.expr())
+        return Node('@', nodeAbs, nodeExpr)
+
+    def visitAbstraccio(self, ctx:hmParser.AbstraccioContext):
+        nodeVar = self.visit(ctx.variable())
+        nodeExpr = self.visit(ctx.expr())
+        return Node('λ', nodeVar, nodeExpr)
+
+    def visitVariable(self, ctx:hmParser.VariableContext):
+        return Node(ctx.getText(), Empty(), Empty())
+
+    def visitNatural(self, ctx:hmParser.NaturalContext):
+        return Node(ctx.getText(), Empty(), Empty())
+
+
+
+
+def displaySemanticTree(t: Tree):
+    def _display(node, graph, parent_id = None):
+        if isinstance(node, Node):
+            node_id = str(id(node))
+            graph.node(node_id, label=node.val)
+
+            if parent_id is not None:
+                graph.edge(parent_id, node_id)
+
+            # Recursive calls
+            _display(node.left, graph, node_id)
+            _display(node.right, graph, node_id)
+
     dot = Graph()
-    dot.node('A', 'A', shape='circle')
-    dot.node('B', 'B', shape='circle')
-    dot.node('C', 'C', shape='circle')
-    dot.edge('A', 'B')
-    dot.edge('A', 'C')
-    return dot
+    _display(t, dot)
 
-# Generar el árbol DOT
+    st.graphviz_chart(dot)
 
-text = st.text_input("Expressió:")
 
-if (st.button("Fer")):
-    lexer = hmLexer(InputStream(text))
+def typeCheck(stream):
+    lexer = hmLexer(stream)
     token_stream = CommonTokenStream(lexer)
     parser = hmParser(token_stream)
     tree = parser.root()
+
     num_errors = parser.getNumberOfSyntaxErrors()
     st.write(f"{num_errors} error/s de sintaxi")
-    
-    dot_tree = generate_dot_tree()
 
-    # Mostrar el árbol en Streamlit
-    st.graphviz_chart(dot_tree.source)
-
+    semantic_builder = SemanticTreeBuilder()
+    semantic_tree = semantic_builder.visit(tree)
+    displaySemanticTree(semantic_tree)
 
 
+def main():
+    text = st.text_input("Expressió:")
+    if (st.button("Fer")):
+        typeCheck(InputStream(text))
+
+
+if __name__ == "__main__":
+    main()
